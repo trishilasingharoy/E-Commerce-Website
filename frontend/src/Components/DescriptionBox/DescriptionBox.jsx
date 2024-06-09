c398f539795fd78222d13cdb8c74167a78aadd42
Your Name <you@example.com>
2024-06-09 12:01:51 +0530

    first comment

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+           <p>An e-commerce website is one that allows people to buy and sell physical goods, services, and digital products over the internet rather than at a brick-and-mortar location. Through an e-commerce website, a business can process orders, accept payments, manage shipping and logistics, and provide customer service.</p>
+           <p>Ecommerce offers people the convenience of shopping from their computers, phones, tablets, and other devices. They visit websites, social media pages, and other virtual channels to find what they’re looking for. Entrepreneurs, startups, small and medium-sized businesses, and large retailers can all use ecommerce to reach customers across the globe. Selling online might be a business’s sole revenue source, or it might be part of a multi-channel selling strategy. For example, a large brick-and-mortar retailer might adopt an online sales channel, or an entrepreneur might sell a small number of specialty handcrafted goods through a social media site like Facebook, Instagram, or Pinterest.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default DescriptionBox
